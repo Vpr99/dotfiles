a5ed1e6ba9a38219216c36a025bc21d89fb910d7
Eric Skram <eric@skram.com>
2015-08-20 11:01:50 -0700

    Drafted list of apps to be installed.

---
+++ apps.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+#==============================================================================
+# Install various applications
+#==============================================================================
+
+export HOMEBREW_CASK_OPTS="--appdir=/Applications"
+
+echo Installing Core apps
+brew cask install 1password
+brew cask install bartender
+brew cask install divvy
+brew cask install dropbox
+brew cask install xtrafinder
+
+echo Installing Development apps
+brew cask install iterm2-nightly
+brew cask install atom
+brew cask install imageoptim
+brew cask install imagealpha
+brew cask install sourcetree
+brew cask install sketch-beta
+brew cask install sketch-toolbox
+
+echo Installing Media apps
+brew cask install google-chrome-canary
+brew cask install spotify
+brew cask install vlc
+brew cask install slack
+
+echo Installing Utilities
+brew cask install appcleaner
+brew cask install xslimmer
+
+
+#==============================================================================
+# App-specific settings & configurations
+#==============================================================================
+sh apps/chrome.sh
+sh apps/transmission.sh
