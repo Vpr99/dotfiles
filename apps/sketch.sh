fc5976f52a5159374ff0b47dda2eff2840734750
Eric Skram <eric@skram.com>
2016-09-25 17:15:02 -0700

    Fix URL for Sketch

--- apps/sketch.sh
+++ apps/sketch.sh
@@ -2,7 +2,7 @@
 # Sketch App configuration
 #==============================================================================
 
-brew cask install sketch-tool
+brew cask install sketch
 
 # Changes the big nudge distance from 10px to 8px
 defaults write ~/Library/Preferences/com.bohemiancoding.sketch3.plist nudgeDistanceBig -int 8
